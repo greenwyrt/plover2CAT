58be5d6d224da356b37ec8e56e132afb2c4a3285
greenwyrt <117969582+greenwyrt@users.noreply.github.com>
2022-11-11 01:22:19 -0500

    Create __init__.py

---
+++ plover_cat/__init__.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QMainWindow, QHBoxLayout
+
+from plover.gui_qt.tool import Tool
+
+from plover_cat.plover_cat_ui import Ui_PloverCAT
+
+from plover_cat.main_window import PloverCATWindow
+
+class PloverCAT(Tool):
+
+    TITLE = "Plover2CAT"
+    ROLE = "plover2cat"
+    ICON = ':/icon.svg'
+    SHORTCUT = "Ctrl+P"
+
+    def __init__(self, engine):
+        super().__init__(engine)
+
+        self.setWindowFlags(Qt.Window | Qt.WindowMinMaxButtonsHint)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(PloverCATWindow(engine))
+        self.layout.setContentsMargins(0,0,0,0)
+        self.setLayout(self.layout)
+
+        #what does this do?
+        self.finished.connect(lambda: None)
