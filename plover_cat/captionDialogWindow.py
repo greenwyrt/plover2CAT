18a204aef7b1341461d1bccdb8c885df84ef75d0
greenwyrt <117969582+greenwyrt@users.noreply.github.com>
2023-08-16 12:42:25 -0700

    trying out obs

--- plover_cat/captionDialogWindow.py
+++ plover_cat/captionDialogWindow.py
@@ -10,6 +10,16 @@ class captionDialogWindow(QDialog, Ui_captionDialog):
         self.font = QFont("Courier New", 12)
         self.fontSelector.clicked.connect(self.set_font)
         self.fontSelector.setText(f"{self.font.family()} {self.font.pointSize()}")
+        self.remoteCapHost.activated.connect(self.enable_host_ui)
+    def enable_host_ui(self, index):
+        if index != 2:
+            self.serverPort.setEnabled(False)
+            self.serverPassword.setEnabled(False)
+            self.hostURL.setEnabled(True)
+        else:
+            self.serverPort.setEnabled(True)
+            self.serverPassword.setEnabled(True)
+            self.hostURL.setEnabled(False)            
     def set_font(self):
         font, ok = QFontDialog.getFont(self.font, self)
         if ok:
