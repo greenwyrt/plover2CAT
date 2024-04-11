47b758a5c991d8c77b889ae17beb15b34c0ff0ec
greenwyrt <117969582+greenwyrt@users.noreply.github.com>
2024-04-10 23:32:32 -0700

    mock for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ source_suffix = {
     '.md': 'markdown',
 }
 
-autodoc_mock_imports = ["plover"]
+autodoc_mock_imports = ["plover", "PyQt5", "dulwich"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
